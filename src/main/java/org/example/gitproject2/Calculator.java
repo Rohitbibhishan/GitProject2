c2517a06026f189316820c9fc352e2fc78ced94a
Elite <rohit.raj.mehta723@gmailcom>
2024-05-05 23:05:33 +0530

    project commit

---
+++ src/main/java/org/example/gitproject2/Calculator.java
@@ -0,0 +1,8 @@
+package org.example.gitproject2;
+
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ src/main/java/org/example/gitproject2/GitProject2Application.java
@@ -0,0 +1,13 @@
+package org.example.gitproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitProject2Application.class, args);
+    }
+
+}

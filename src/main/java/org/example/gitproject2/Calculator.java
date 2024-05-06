44c6815edb10dc0c9619c9625f4c17272779955a
Elite <rohit.raj.mehta723@gmailcom>
2024-05-06 19:50:24 +0530

    checking conflict

--- src/main/java/org/example/gitproject2/Calculator.java
+++ src/main/java/org/example/gitproject2/Calculator.java
@@ -2,7 +2,8 @@ package org.example.gitproject2;
 
 public class Calculator {
     public int sum(int a, int b) {
-        return a + b+20;
+        return a + b+30;
+        //return stmnt
     }
 
 }
